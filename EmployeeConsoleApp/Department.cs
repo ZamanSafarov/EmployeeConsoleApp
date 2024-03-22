48919b3134bd8149b7948e48c094d3ceaec429e8
Zaman Safarov <zamanjs@code.edu.az>
2024-03-22 06:49:27 +0400

    first commit

---
+++ EmployeeConsoleApp/Department.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeConsoleApp
+{
+    public class Department
+    {
+        Employee[] Employees = new Employee[] { };
+
+        public void AddEmployee(Employee employee)
+        {
+            Array.Resize(ref Employees, Employees.Length + 1);
+
+            Employees[Employees.Length-1] = employee;
+        }
+        public void ShowEmployeeInfo()
+        {
+            foreach (Employee emp in Employees) {
+                Console.WriteLine($"Name: {emp.Name}\nSurname: {emp.Surname}\nAge: {emp.Age}\nDepartmentName:{emp.DepartmentName}\nSalary: {emp.Salary}");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+            }
+
+        }
+        public Employee[] GetAllEmployees()
+        {
+            return Employees;
+        }
+        public void GetAllEmployeesBySalary(int minSalary, int maxSalar)
+        {
+            if (Employees.Length !=0 )
+            {
+                foreach (Employee emp in Employees)
+                {
+                    if (emp.Salary > minSalary && emp.Salary < maxSalar)
+                    {
+                        Console.WriteLine($"Name: {emp.Name}\nSurname: {emp.Surname}\nAge: {emp.Age}\nDepartmentName:{emp.DepartmentName}\nSalary: {emp.Salary}");
+                        Console.WriteLine("---------------------------------------------------------------------------------------");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bu maaş aralığında işçi yoxdur!!");
+                        break;
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Işçi yoxdur!!");
+            }
+            
+                
+          
+        }
+    }
+}
---
+++ EmployeeConsoleApp/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeConsoleApp
+{
+    public class Employee
+    {
+        public Employee(string name,string surname,byte age,string departmentNo,int salary)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            DepartmentName = departmentNo;
+            Salary = salary;
+
+        }
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string DepartmentName;
+        public int Salary;
+    }
+}
